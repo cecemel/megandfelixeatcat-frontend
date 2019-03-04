5f1469bc1f6d17e4c91b5e6be221c132ccfa0623
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-03-04 16:50:55 +0100

    added foto url

--- app/components/food-establishment-detail-view.js
+++ app/components/food-establishment-detail-view.js
@@ -1,4 +1,9 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+   photoUrl: computed('establishment.photos.[]', function(){
+    if(!this.establishment.photos.firstObject) return'';
+    return `/files/${this.establishment.photos.firstObject.id}/download`;
+  })
 });
